d806fa1145ebde087e694ac954d231fe6191e7ba
JetMas <jethromasangya@gmail.com>
2018-03-28 22:54:48 -0400

    Styling with bootstrap 4. Alot of changes to the RSS file.

--- js/newsRSS.js
+++ js/newsRSS.js
@@ -1,39 +1,68 @@
+
 var handleData = function(data) {
   // data is a DOM-like object, so `querySelectorAll` works
   const items = Array.from(data.querySelectorAll('item'));
 
   // map the list of items into nodes for html
   const nodes = items.map(function(item) {
+    //Get node in html with the proper bootstrap classes
+    const articleNode = document.getElementById('article').cloneNode(true);
+
     // make a node of the image
-    const imageNode = document.createElement('img');
+    const imageNode = articleNode.querySelector('img[id="image"]');
+    //imageNode.className = 'img-thumbnail'
+    //imageNode.setAttribute('id', 'articleImage');
     const mediaContent = item.getElementsByTagName('media:content')[0];
     //Some stories have images and some don't
     if (mediaContent){
       imageNode.src = mediaContent.getAttribute('url');
     }
+    else {
+      imageNode.src = 'images/placeHolder.jpg';
+    }
+    //articleNode.querySelector('div[id="imageWrapper"]').innerHTML = '';
+    //articleNode.querySelector('div[id="imageWrapper"]').appendChild(imageNode);
 
     // make a node for the title
-    const titleNode = document.createElement('strong');
-    titleNode.innerHTML = item.querySelector('title').textContent +
-      '&nbsp;';
+    const titleNode = articleNode.querySelector('a[id="title"]');
+    titleNode.href = item.querySelector('link').textContent;
+    titleNode.innerHTML = item.querySelector('title').textContent + '&nbsp;';
+    //articleNode.querySelector('div[id="title"]').innerHTML = '';
+    //articleNode.querySelector('div[id="title"]').appendChild(titleNode);
 
     // make a node for the date
-    const dateNode = document.createElement('em');
-    dateNode.innerHTML = item.querySelector('pubDate').textContent +
-      '&nbsp;';
+    const dateNode = articleNode.querySelector('div[id="pubDate"]');
+    dateNode.innerHTML = item.querySelector('pubDate').textContent + '&nbsp;';
+    //articleNode.querySelector('div[id="pubDate"]').innerHTML = item.querySelector('pubDate').textContent;
+    //articleNode.querySelector('div[id="pubDate"]').appendChild(dateNode);
+
+    // make a node for the description
+    const descriptionNode = articleNode.querySelector('p[id="description"]');
+    descriptionNode.innerHTML = item.querySelector('description').textContent + '&nbsp;';
+    //articleNode.querySelector('div[id="description"]').innerHTML = item.querySelector('description').textContent;
+    //articleNode.querySelector('div[id="description"]').appendChild(descriptionNode);
 
     // make a node for the link
-    const linkNode = document.createElement('a');
+    const linkNode = articleNode.querySelector('a[id="link"]');
     linkNode.href = item.querySelector('link').textContent;
-    linkNode.textContent = '(link)';
+    //linkNode.textContent = '(link)';
+    //articleNode.querySelector('div[id="description"]').appendChild(linkNode);
+
+
+    /*
+    const infoNode = document.createElement('div');
+    infoNode.className = 'col-md-10';
+    infoNode.appendChild(titleNode);
+    infoNode.appendChild(dateNode);
+    infoNode.appendChild(descriptionWrap);
+    //infoNode.appendChild(linkNode);
 
     // make a node for everything
-    const articleNode = document.createElement('article');
-    articleNode.appendChild(imageNode);
-    articleNode.appendChild(titleNode);
-    articleNode.appendChild(dateNode);
-    articleNode.appendChild(linkNode);
-
+    const articleNode = document.createElement('div');
+    articleNode.className = 'row justify-content-md-center';
+    articleNode.appendChild(imageWrap);
+    articleNode.appendChild(infoNode);
+    */
     return articleNode;
   });
 
