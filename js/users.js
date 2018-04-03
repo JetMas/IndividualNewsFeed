f09e15bae4a8740087e88b8b9a73227b26e6eefc
JetMas <jethromasangya@gmail.com>
2018-04-03 15:01:54 -0400

    Added some logic to the login.

--- js/users.js
+++ js/users.js
@@ -1,18 +1,31 @@
 const userFile = 'json/users.json';
 
+function setUserCookie(username){
+  document.cookie = `user=${username}; path=/`;
+}
+
 function login(username, password) {
   $.getJSON(userFile, function(data){
-    console.log(data);
+    //console.log(data);
     $.each(data, function(key, val) {
-      console.log(`${key} : ${val.username}`);
+      console.log(`${val.username}`);
+      //Check if the username match
+      //then check if the password match
+      if( (val.username.toUpperCase() === username.toUpperCase()) && (val.password == password) ){
+        //setUserCookie(username);
+        console.log("login success!")
+      }
     });
   });
 }
 
 
 $(document).ready(function () {
+
   $('#login').click(function () {
     //console.log('click1');
-    login('user', 1234);
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+    login(username, password);
   });
 });
