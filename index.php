bfc7ddaf82026d8a1cdf38ff75c82b076759ef6a
JetMas <jethromasangya@gmail.com>
2018-04-17 18:20:01 -0400

    Fixed echo in index

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ session_start();
   </div>
   <?php 
     if($_SESSION['user'] != ''){
-      echo'<label class="text-white">'+$_SESSION['user']+'</label>';
+      echo('<label class="text-white">'+$_SESSION['user']+'</label>');
       echo '<button class="btn btn-outline-success" id="logout">Logout</button>';
     }
     else {
