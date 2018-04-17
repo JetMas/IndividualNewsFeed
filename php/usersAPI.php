3fa85b6952e9431b47b0bb256677421574fcc352
JetMas <jethromasangya@gmail.com>
2018-04-17 17:50:25 -0400

    Debugging

--- php/usersAPI.php
+++ php/usersAPI.php
@@ -6,8 +6,7 @@ session_start();
 $users_file_path = "users.json";
 $users_str = file_get_contents($users_file_path);
 $users_array = json_decode($users_str, true);
-print_r($users_str);
-print_r("\n");  
+  
 
 function get_users() {
     return $users;
@@ -27,10 +26,9 @@ function get_user_by_username($username){
             "Address":{"Street":"7 Yuki Street","City":"Tokyo"}
         }
        ]';
-    print_r($sample_data);
+    print_r($GLOBALS['users_str']);
     print_r("\n");
-    $sample_array = json_decode($sample_data, true);
-    print_r(count($sample_array));     
+    $sample_array = json_decode($sample_data, true); 
     foreach ($users_array as $key1 => $value1) {
         print_r($users_array[$key1]);
     }
