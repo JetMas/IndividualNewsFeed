55c3b7ac2b3abf262516c826e5176ec038ed01cb
JetMas <jethromasangya@gmail.com>
2018-04-17 17:10:53 -0400

    Debugging

--- php/usersAPI.php
+++ php/usersAPI.php
@@ -5,9 +5,8 @@ session_start();
 
 $users_file_path = "users.json";
 $users_str = file_get_contents($users_file_path);
-var_export($users_str);
-$users_array = json_decode($users_str, true);
-var_export($users);
+$users_array = json_decode($users_str);
+print_r($users);
 
 function get_users() {
     return $users;
@@ -15,10 +14,10 @@ function get_users() {
 
 function get_user_by_username($username){
 	var_export($username);
-    foreach ($users_array as $user){
-	    var_export($user);
-        if(strcasecmp($user['username'],$username) == 0){
-            return $user;
+    foreach ($users_array as $key => $value){
+	    var_export($value);
+        if(strcasecmp($value['username'],$username) == 0){
+            return $value;
         }
     }
     return null;
