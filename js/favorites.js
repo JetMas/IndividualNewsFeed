fd255413369e3b427cf3af244f760003dc456aab
JetMas <jethromasangya@gmail.com>
2018-04-18 13:02:28 -0400

    Added some favorite functionality.

---
+++ js/favorites.js
@@ -0,0 +1,9 @@
+
+
+$(document).ready(function () {
+  $('#favoriteButton').click(function () {
+    //console.log('click1');   
+    console.log($(this).closest("#title").innerHTML);
+    console.log($(this).closest("#link").href);
+  });
+});
