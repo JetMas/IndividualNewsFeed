a51c0d095d01157e5b2fc6a48fe66820858876f4
JetMas <jethromasangya@gmail.com>
2018-04-18 13:19:18 -0400

    Debugging

--- js/favorites.js
+++ js/favorites.js
@@ -3,7 +3,7 @@
 
 $(document).ready(function () {
     console.log("Adding favorite onlcick");
-    $('.favoriteButton').click(function () {
+    $(".favoriteButton").click(function () {
         console.log('click1');   
         console.log($(this).closest("#title").innerHTML);
         console.log($(this).closest("#link").href);
