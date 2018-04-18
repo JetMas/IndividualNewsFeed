21c66505fbb1c3f3cd1387d31d6c638313ece9d2
JetMas <jethromasangya@gmail.com>
2018-04-18 14:39:05 -0400

    Bug fixes

--- js/favorites.js
+++ js/favorites.js
@@ -3,6 +3,6 @@ function favorite(element){
     console.log(element);
     console.log($(element).closest("#articleInfo").find("#title"));
     console.log($(element).closest("#articleInfo").find("#link"));
-    console.log($(element).closest("#articleInfo").find("#title").textContent);
+    console.log($(element).closest("#articleInfo").find("#title").attr('text'));
     console.log($(element).closest("#articleInfo").find("#link").attr('href'));
 }
