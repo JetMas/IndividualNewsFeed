4a01b3a443c0872f0c723f300b75caf865b609b1
JetMas <jethromasangya@gmail.com>
2018-04-18 00:27:38 -0400

    Added some favorites API methods.

---
+++ php/favoritesAPI.php
@@ -0,0 +1,65 @@
+<?php
+//Author: Jethro Masangya
+//Description: Provides Api for accessing the users json file.
+session_start();
+
+$favorites_file_path = "favorites.json";
+$favorites_str = file_get_contents($favorites_file_path);
+$favorites_array = json_decode($favorites_str, true);
+  
+
+function get_favorites($username) {
+    $favorites = array();
+    foreach($GLOBALS['favorites_array'] as $favorite){
+        if(strcasecmp($favorite['username'], $username) == 0){
+            array_push($favorites, $favorite);
+        }
+    }
+    return $favorites;
+}
+
+
+//Creates a new user
+//endcodes the user info and adds it to users.json.
+function new_favorite($username, $title, $url){
+
+}
+
+$accepted_URL = array("get_favorites", "new_favorite");
+$value = "An error has occured";
+
+if(isset($_GET["action"]) && in_array($_GET["action"], $accepted_URL)){
+    switch($_GET["action"]){
+        case "get_favorites":
+            if($_SESSION['user'] != ''){
+                $value = get_favorites($_SESSION['user']);
+            }
+            else {
+                $value = "Must be logged in to favorite stories.";
+            }
+            break;
+        default:
+            $value = "Unkown action.";            
+    }
+}
+if(isset($_POST["action"]) && in_array($_POST["action"], $accepted_URL)){
+    switch($_POST["action"]){
+        case "new_favorite":
+            if(isset($_POST["title"]) && isset($_POST["url"])){
+                if($_SESSION['user'] != ''){
+                    $value = new_favorite($_SESSION['user'], $_POST["title"], $_POST["url"]);
+                }
+                else {
+                    $value = "Must be logged in to create new favorite.";
+                }
+            }
+            else {
+                $value = "Missing argument";                
+            }
+            break;
+        default:
+            $value = "Unkown action.";            
+    }
+}
+exit(json_encode($value));
+?>
